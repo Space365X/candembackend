34d8359b3c343d5aa0ae56d406d8493329dc4310
OliverRosie <141502150+WizardOfCodes442@users.noreply.github.com>
2025-03-12 06:24:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  # Allow your frontend origin
+    allow_origins=["https://candembank.com"],  # Allow your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
